ad1ea084cc63e646b4d2c233a2d9c0f72a327550
Jay Price <jaypventures@icloud.com>
2026-09-10 12:58:54 -0400

    feat: register reciprocity ledger source

--- src/JPVOS/Services/Reciprocity/ReciprocityServiceCollectionExtensions.cs
+++ src/JPVOS/Services/Reciprocity/ReciprocityServiceCollectionExtensions.cs
@@ -4,10 +4,11 @@ namespace JPVOS.Services.Reciprocity;
 
 public static class ReciprocityServiceCollectionExtensions
 {
-    public static IServiceCollection AddJpvReciprocityGate(this IServiceCollection services, string auditPath)
+    public static IServiceCollection AddJpvReciprocityGate(this IServiceCollection services, string ledgerPath, string auditPath)
     {
         services.AddSingleton<ReciprocityEvaluator>();
         services.AddSingleton<ReciprocityAdmissionGate>();
+        services.AddSingleton(new ReciprocityLedgerStore(ledgerPath));
         services.AddSingleton(new ReciprocityAuditStore(auditPath));
         services.AddSingleton<ReciprocityEnforcementService>();
         return services;
